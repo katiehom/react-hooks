f999ef196197da0acfe13dd94390d2523ab1d634
Katie Hom <schaka01@gmail.com>
2023-01-30 10:20:02 -0700

    Exercise 2 Hooks and Custom Hooks

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -1,20 +1,222 @@
 // useEffect: persistent state
 // http://localhost:3000/isolated/exercise/02.js
 
+// import * as React from 'react'
+
+// function Greeting({initialName = ''}) {
+//   // 🐨 initialize the state to the value from localStorage
+//   // 💰 window.localStorage.getItem('name') ?? initialName
+//   const [name, setName] = React.useState(
+//     window.localStorage.getItem('name') ?? initialName,
+//   )
+
+//   // 🐨 Here's where you'll use `React.useEffect`.
+//   // The callback should set the `name` in localStorage.
+//   // 💰 window.localStorage.setItem('name', name)
+//   React.useEffect(() => {
+//     window.localStorage.setItem('name', name)
+//   })
+
+//   function handleChange(event) {
+//     setName(event.target.value)
+//   }
+
+//   return (
+//     <div>
+//       <form>
+//         <label htmlFor="name">Name: </label>
+//         <input value={name} onChange={handleChange} id="name" />
+//       </form>
+//       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+//     </div>
+//   )
+// }
+
+// function App() {
+//   return <Greeting />
+// }
+
+// export default App
+
+// Extra Credit 1
+// import * as React from 'react'
+
+// function Greeting({initialName = ''}) {
+//   const [name, setName] = React.useState(
+//     () => window.localStorage.getItem('name') ?? initialName,
+//   )
+
+//   React.useEffect(() => {
+//     window.localStorage.setItem('name', name)
+//   })
+
+//   function handleChange(event) {
+//     setName(event.target.value)
+//   }
+
+//   return (
+//     <div>
+//       <form>
+//         <label htmlFor="name">Name: </label>
+//         <input value={name} onChange={handleChange} id="name" />
+//       </form>
+//       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+//     </div>
+//   )
+// }
+
+// function App() {
+//   return <Greeting />
+// }
+
+// export default App
+
+// Extra Credit 2
+// import * as React from 'react'
+
+// function Greeting({initialName = ''}) {
+//   const [name, setName] = React.useState(
+//     () => window.localStorage.getItem('name') ?? initialName,
+//   )
+
+//   React.useEffect(() => {
+//     window.localStorage.setItem('name', name)
+//   }, [name])
+
+//   function handleChange(event) {
+//     setName(event.target.value)
+//   }
+
+//   return (
+//     <div>
+//       <form>
+//         <label htmlFor="name">Name: </label>
+//         <input value={name} onChange={handleChange} id="name" />
+//       </form>
+//       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+//     </div>
+//   )
+// }
+
+// function App() {
+//   return <Greeting />
+// }
+
+// export default App
+
+// Extra Credit 3
+// import * as React from 'react'
+
+// function useLocalStorageState(initialName) {
+//   const [name, setName] = React.useState(
+//     () => window.localStorage.getItem('name') ?? initialName,
+//   )
+//   React.useEffect(() => {
+//     window.localStorage.setItem('name', name)
+//   }, [name])
+
+//   return [name, setName]
+// }
+
+// function Greeting({initialName = ''}) {
+//   const [name, setName] = useLocalStorageState()
+
+//   function handleChange(event) {
+//     setName(event.target.value)
+//   }
+
+//   return (
+//     <div>
+//       <form>
+//         <label htmlFor="name">Name: </label>
+//         <input value={name} onChange={handleChange} id="name" />
+//       </form>
+//       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+//     </div>
+//   )
+// }
+
+// function App() {
+//   return <Greeting />
+// }
+
+// export default App
+
+// Extra Credit 3 -- Refactor custom hook to be more reusable
+// import * as React from 'react'
+
+// function useLocalStorageState(key, defaultValue = '') {
+//   const [state, setState] = React.useState(
+//     () => window.localStorage.getItem(key) ?? defaultValue,
+//   )
+//   React.useEffect(() => {
+//     window.localStorage.setItem(key, state)
+//   }, [key, state])
+
+//   return [state, setState]
+// }
+
+// function Greeting({initialName = ''}) {
+//   const [name, setName] = useLocalStorageState('name', initialName)
+
+//   function handleChange(event) {
+//     setName(event.target.value)
+//   }
+
+//   return (
+//     <div>
+//       <form>
+//         <label htmlFor="name">Name: </label>
+//         <input value={name} onChange={handleChange} id="name" />
+//       </form>
+//       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+//     </div>
+//   )
+// }
+
+// function App() {
+//   return <Greeting />
+// }
+
+// export default App
+
+// Extra Credit 4 - Make custom hook generic to support any data type
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+function useLocalStorageState(
+  key,
+  defaultValue = '',
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) {
+  const [state, setState] = React.useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key)
+    if (valueInLocalStorage) {
+      return deserialize(valueInLocalStorage)
+    }
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+  })
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+  const prevKeyRef = React.useRef(key)
+
+  React.useEffect(() => {
+    const prevKey = prevKeyRef.current
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey)
+    }
+    prevKeyRef.current = key
+    window.localStorage.setItem(key, serialize(state))
+  }, [key, serialize, state])
+
+  return [state, setState]
+}
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageState('name', initialName)
 
   function handleChange(event) {
     setName(event.target.value)
   }
+
   return (
     <div>
       <form>
