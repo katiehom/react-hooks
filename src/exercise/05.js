7002f6ed8db9928d18d3f0636c3265df0782c84b
Katie Hom <schaka01@gmail.com>
2023-03-21 13:14:44 -0600

    Exercise 5 review

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,6 +1,9 @@
 // useRef and useEffect: DOM interaction
 // http://localhost:3000/isolated/exercise/05.js
 
+//////////////////////////////////////////////
+// REVIEW DOM interaction
+
 import * as React from 'react'
 // eslint-disable-next-line no-unused-vars
 import VanillaTilt from 'vanilla-tilt'
@@ -38,3 +41,43 @@ function App() {
 }
 
 export default App
+
+////////////////////////
+
+// import * as React from 'react'
+// // eslint-disable-next-line no-unused-vars
+// import VanillaTilt from 'vanilla-tilt'
+
+// function Tilt({children}) {
+//   const tiltRef = React.useRef()
+
+//   React.useEffect(() => {
+//     const tiltNode = tiltRef.current
+//     VanillaTilt.init(tiltNode, {
+//       max: 25,
+//       speed: 400,
+//       glare: true,
+//       'max-glare': 0.5,
+//     })
+
+//     return function cleanup() {
+//       tiltNode.vanillaTilt.destroy()
+//     }
+//   }, [])
+
+//   return (
+//     <div ref={tiltRef} className="tilt-root">
+//       <div className="tilt-child">{children}</div>
+//     </div>
+//   )
+// }
+
+// function App() {
+//   return (
+//     <Tilt>
+//       <div className="totally-centered">vanilla-tilt.js</div>
+//     </Tilt>
+//   )
+// }
+
+// export default App
