0da866a23933c0317bc8d52a69e761d52a3a18a0
Katie Hom <schaka01@gmail.com>
2023-03-21 13:13:32 -0600

    Exercise 4 review

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -228,13 +228,128 @@
 
 // export default App
 
-//
+// Extra Credit 2 - useLocalStorageState Custom Hook
+// import * as React from 'react'
+// import {useLocalStorageState} from '../utils'
 
-// Extra Credit 3 - add game history feature
+// function Board() {
+//   const [squares, setSquares] = useLocalStorageState(
+//     'squares',
+//     Array(9).fill(null),
+//   )
+
+//   const nextValue = calculateNextValue(squares)
+//   const winner = calculateWinner(squares)
+//   const status = calculateStatus(winner, squares, nextValue)
+
+//   function selectSquare(square) {
+//     if (winner || squares[square]) {
+//       return
+//     }
+
+//     const squaresCopy = [...squares]
+//     squaresCopy[square] = nextValue
+//     setSquares(squaresCopy)
+//   }
+
+//   function restart() {
+//     setSquares(Array(9).fill(null))
+//   }
+
+//   function renderSquare(i) {
+//     return (
+//       <button className="square" onClick={() => selectSquare(i)}>
+//         {squares[i]}
+//       </button>
+//     )
+//   }
+
+//   return (
+//     <div>
+//       {/* 🐨 put the status in the div below */}
+//       <div className="status">{status}</div>
+//       <div className="board-row">
+//         {renderSquare(0)}
+//         {renderSquare(1)}
+//         {renderSquare(2)}
+//       </div>
+//       <div className="board-row">
+//         {renderSquare(3)}
+//         {renderSquare(4)}
+//         {renderSquare(5)}
+//       </div>
+//       <div className="board-row">
+//         {renderSquare(6)}
+//         {renderSquare(7)}
+//         {renderSquare(8)}
+//       </div>
+//       <button className="restart" onClick={restart}>
+//         restart
+//       </button>
+//     </div>
+//   )
+// }
+
+// function Game() {
+//   return (
+//     <div className="game">
+//       <div className="game-board">
+//         <Board />
+//       </div>
+//     </div>
+//   )
+// }
+
+// // eslint-disable-next-line no-unused-vars
+// function calculateStatus(winner, squares, nextValue) {
+//   return winner
+//     ? `Winner: ${winner}`
+//     : squares.every(Boolean)
+//     ? `Scratch: Cat's game`
+//     : `Next player: ${nextValue}`
+// }
+
+// // * Determine whose turn it is based on number of 'x' and 'o' squares
+// // eslint-disable-next-line no-unused-vars
+// function calculateNextValue(squares) {
+//   return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O'
+// }
+
+// // * Determined based on the value of 'squares' from 'calculateWinner' func
+// // eslint-disable-next-line no-unused-vars
+// function calculateWinner(squares) {
+//   const lines = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6],
+//   ]
+//   for (let i = 0; i < lines.length; i++) {
+//     const [a, b, c] = lines[i]
+//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+//       return squares[a]
+//     }
+//   }
+//   return null
+// }
+
+// function App() {
+//   return <Game />
+// }
+
+// export default App
+
+//////////////////////////////////////////////////////////
+
+// Extra Credit 3.2- reviewing add game history feature
 import * as React from 'react'
 import {useLocalStorageState} from '../utils'
 
-function Board({onClick, squares}) {
+function Board({squares, onClick}) {
   function renderSquare(i) {
     return (
       <button className="square" onClick={() => onClick(i)}>
@@ -269,6 +384,7 @@ function Game() {
     'tic-tac-toe:step',
     0,
   )
+
   const [history, setHistory] = useLocalStorageState('tic-tac-toe:history', [
     Array(9).fill(null),
   ])
@@ -282,12 +398,11 @@ function Game() {
     if (winner || currentSquares[square]) {
       return
     }
-
     const newHistory = history.slice(0, currentStep + 1)
-    const squaresCopy = [...currentSquares]
-    squaresCopy[square] = nextValue
+    const squares = [...currentSquares]
+    squares[square] = nextValue
 
-    setHistory([...newHistory, squaresCopy])
+    setHistory([...newHistory, squares])
     setCurrentStep(newHistory.length)
   }
 
@@ -297,7 +412,7 @@ function Game() {
   }
 
   const moves = history.map((stepSquares, step) => {
-    const desc = step === 0 ? 'Go to game start' : `Go to move #${step}`
+    const desc = step ? `Go to move #${step}` : 'Go to game start'
     const isCurrentStep = step === currentStep
     return (
       <li key={step}>
@@ -366,3 +481,142 @@ function App() {
 }
 
 export default App
+
+////////////////////////////////////////////////////////
+
+// // Extra Credit 3 - add game history feature
+// import * as React from 'react'
+// import {useLocalStorageState} from '../utils'
+
+// function Board({onClick, squares}) {
+//   function renderSquare(i) {
+//     return (
+//       <button className="square" onClick={() => onClick(i)}>
+//         {squares[i]}
+//       </button>
+//     )
+//   }
+
+//   return (
+//     <div>
+//       <div className="board-row">
+//         {renderSquare(0)}
+//         {renderSquare(1)}
+//         {renderSquare(2)}
+//       </div>
+//       <div className="board-row">
+//         {renderSquare(3)}
+//         {renderSquare(4)}
+//         {renderSquare(5)}
+//       </div>
+//       <div className="board-row">
+//         {renderSquare(6)}
+//         {renderSquare(7)}
+//         {renderSquare(8)}
+//       </div>
+//     </div>
+//   )
+// }
+
+// function Game() {
+//   const [currentStep, setCurrentStep] = useLocalStorageState(
+//     'tic-tac-toe:step',
+//     0,
+//   )
+//   const [history, setHistory] = useLocalStorageState('tic-tac-toe:history', [
+//     Array(9).fill(null),
+//   ])
+
+//   const currentSquares = history[currentStep]
+//   const nextValue = calculateNextValue(currentSquares)
+//   const winner = calculateWinner(currentSquares)
+//   const status = calculateStatus(winner, currentSquares, nextValue)
+
+//   function selectSquare(square) {
+//     if (winner || currentSquares[square]) {
+//       return
+//     }
+
+//     const newHistory = history.slice(0, currentStep + 1)
+//     const squaresCopy = [...currentSquares]
+//     squaresCopy[square] = nextValue
+
+//     setHistory([...newHistory, squaresCopy])
+//     setCurrentStep(newHistory.length)
+//   }
+
+//   function restart() {
+//     setHistory([Array(9).fill(null)])
+//     setCurrentStep(0)
+//   }
+
+//   const moves = history.map((stepSquares, step) => {
+//     const desc = step === 0 ? 'Go to game start' : `Go to move #${step}`
+//     const isCurrentStep = step === currentStep
+//     return (
+//       <li key={step}>
+//         <button disabled={isCurrentStep} onClick={() => setCurrentStep(step)}>
+//           {desc} {isCurrentStep ? '(current)' : null}
+//         </button>
+//       </li>
+//     )
+//   })
+
+//   return (
+//     <div className="game">
+//       <div className="game-board">
+//         <Board onClick={selectSquare} squares={currentSquares} />
+//         <button className="restart" onClick={restart}>
+//           restart
+//         </button>
+//       </div>
+//       <div className="game-info">
+//         <div>{status}</div>
+//         <ol>{moves}</ol>
+//       </div>
+//     </div>
+//   )
+// }
+
+// // eslint-disable-next-line no-unused-vars
+// function calculateStatus(winner, squares, nextValue) {
+//   return winner
+//     ? `Winner: ${winner}`
+//     : squares.every(Boolean)
+//     ? `Scratch: Cat's game`
+//     : `Next player: ${nextValue}`
+// }
+
+// // * Determine whose turn it is based on number of 'x' and 'o' squares
+// // eslint-disable-next-line no-unused-vars
+// function calculateNextValue(squares) {
+//   return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O'
+// }
+
+// // * Determined based on the value of 'squares' from 'calculateWinner' func
+// // eslint-disable-next-line no-unused-vars
+// function calculateWinner(squares) {
+//   const lines = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6],
+//   ]
+//   for (let i = 0; i < lines.length; i++) {
+//     const [a, b, c] = lines[i]
+//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+//       return squares[a]
+//     }
+//   }
+//   return null
+// }
+
+// function App() {
+//   return <Game />
+// }
+
+// export default App
