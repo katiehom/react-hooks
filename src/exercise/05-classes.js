5204eb9b51aff207756178d45d2ee0fa1ad1c02c
Katie Hom <schaka01@gmail.com>
2023-03-21 13:14:27 -0600

    Exercise 5 - refactor class component to function component

--- src/exercise/05-classes.js
+++ src/exercise/05-classes.js
@@ -2,16 +2,55 @@
 // 💯 (alternate) migrate from classes
 // http://localhost:3000/isolated/exercise/05-classes.js
 
-import * as React from 'react'
-import VanillaTilt from 'vanilla-tilt'
+// import * as React from 'react'
+// import VanillaTilt from 'vanilla-tilt'
 
 // If you'd rather practice refactoring a class component to a function
 // component with hooks, then go ahead and do this exercise.
 
-class Tilt extends React.Component {
-  tiltRef = React.createRef()
-  componentDidMount() {
-    const tiltNode = this.tiltRef.current
+// class Tilt extends React.Component {
+//   tiltRef = React.createRef()
+//   componentDidMount() {
+//     const tiltNode = this.tiltRef.current
+//     const vanillaTiltOptions = {
+//       max: 25,
+//       speed: 400,
+//       glare: true,
+//       'max-glare': 0.5,
+//     }
+//     VanillaTilt.init(tiltNode, vanillaTiltOptions)
+//   }
+//   componentWillUnmount() {
+//     this.tiltRef.current.vanillaTilt.destroy()
+//   }
+//   render() {
+//     return (
+//       <div ref={this.tiltRef} className="tilt-root">
+//         <div className="tilt-child">{this.props.children}</div>
+//       </div>
+//     )
+//   }
+// }
+// function App() {
+//   return (
+//     <Tilt>
+//       <div className="totally-centered">vanilla-tilt.js</div>
+//     </Tilt>
+//   )
+// }
+
+// export default App
+
+// REFACTOR from CLASSES
+
+import * as React from 'react'
+import VanillaTilt from 'vanilla-tilt'
+
+function Tilt({children}) {
+  const tiltRef = React.useRef()
+
+  React.useEffect(() => {
+    const tiltNode = tiltRef.current
     const vanillaTiltOptions = {
       max: 25,
       speed: 400,
@@ -19,18 +58,16 @@ class Tilt extends React.Component {
       'max-glare': 0.5,
     }
     VanillaTilt.init(tiltNode, vanillaTiltOptions)
-  }
-  componentWillUnmount() {
-    this.tiltRef.current.vanillaTilt.destroy()
-  }
-  render() {
-    return (
-      <div ref={this.tiltRef} className="tilt-root">
-        <div className="tilt-child">{this.props.children}</div>
-      </div>
-    )
-  }
+    return () => tiltNode.vanillaTilt.destroy()
+  }, [])
+
+  return (
+    <div ref={tiltRef} className="tilt-root">
+      <div className="tilt-child">{children}</div>
+    </div>
+  )
 }
+
 function App() {
   return (
     <Tilt>
